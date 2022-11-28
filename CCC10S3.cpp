d6fff63f8c7715560403ee1fc37ff641df8f5375
pirate2580 <90294883+pirate2580@users.noreply.github.com>
2022-11-27 20:10:51 -0500

    Add files via upload

---
+++ CCC10S3.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+int main()
+{
+    int T;cin>>T;
+    while (T--)
+    {
+        int leaf [5][5]={{0,0,0,0,0},{0,0,0,0,0},{0,0,0,0,0},{0,0,1,0,0},{0,1,1,1,0}};
+
+    }
+    return 0;
+}
